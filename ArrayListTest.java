77a091cd7d6a6fb765674f2d7a2ee23f29c5adcb
Juyoung Lee <leejuyoung@gpa.ac.kr>
2015-10-27 13:26:22 +0900

    Create ArrayListTest.java

---
+++ ArrayListTest.java
@@ -0,0 +1,16 @@
+/*
+* Project: ArrayListTest.java
+* Description: Playing with ArrayLists
+* Name: Juyoung Lee
+* Date: Oct 27, 2015
+*/
+
+import java.util.ArrayList;
+
+public class ArrayListsTest {
+  
+  public static void main( String[] args ) {
+  
+  } // End main method
+  
+  } // End class
