4a1a476ba93fe0cac353acf7ceaeba11821fc5a9
Juyoung Lee <leejuyoung@gpa.ac.kr>
2015-10-27 13:35:54 +0900

    Update ArrayListTest.java

--- ArrayListTest.java
+++ ArrayListTest.java
@@ -9,8 +9,35 @@ import java.util.ArrayList;
 
 public class ArrayListsTest {
   
+  public static printList( ArrayList a ) {
+    
+    System.out.println( "The list size is now: " + a.size(); + " elements long." );
+    for (int i=0; i<a.size(); i++ ) {
+      System.out.println( a.get(i) + ", " );
+    }
+    
+  } // End pringList()
+  
   public static void main( String[] args ) {
   
+  ArrayList<Interger> list = new ArrayList<Interger>();
+  
+  list.add( 3 );
+  list.add( 6 );
+  list.add( 9 );
+  list.add( 13 );
+  list.add( 16 );
+  // [ 3, 6, 9, 13, 16 ] length: 5
+  printList( list );
+  
+  list.remove( 1 );    //[3,9,13,16]
+  list.remove( 3 );    //[3,6,13]
+  list.add( 19 );      //[3,6,13,19]
+  list.add( 23 );      //[3,6,13,19,23]
+  list.add( 2,235 );   //[3,6,235,13,19,23]
+  list.set( 0,17);     //[3,6,235,13,19,23]
+  printlist( list );
+     
   } // End main method
   
   } // End class
